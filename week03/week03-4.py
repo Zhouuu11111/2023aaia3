d4c8b04d38db04f795560e067e08fc5a47449b0a
賴勁舟 <91170808+Zhouuu11111@users.noreply.github.com>
2024-09-26 16:15:27 +0800

    Add files via upload

---
+++ week03/week03-4.py
@@ -0,0 +1,9 @@
+```
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        //https://cplusplus.com/reference/string/string/find/#google_vignette
+        return haystack.find(needle);
+    }
+};
+```
