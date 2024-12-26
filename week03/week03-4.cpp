c0bd0b77fab25fa748e56d3d9eb321adb5e8a6f2
賴勁舟 <91170808+Zhouuu11111@users.noreply.github.com>
2024-12-26 14:48:21 +0800

    Delete week03 directory

--- week03/week03-4.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    int strStr(string haystack, string needle) {
-        //https://cplusplus.com/reference/string/string/find/#google_vignette
-        return haystack.find(needle);
-    }
-};
--- week03/week03-5.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    bool repeatedSubstringPattern(string s) {
-        string s2 = s + s;
-        int N = s2.length();
-        return s2.substr(1,N-2).find(s) != string::npos;
-    }
-};
